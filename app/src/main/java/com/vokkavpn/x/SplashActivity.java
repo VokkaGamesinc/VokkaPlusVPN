50b6705aceeccc54ee4e13ef5196fb2e805a0d49
globalsend-inc <kashif@globalsend.us>
2020-11-14 16:01:17 +0400

    Permission

--- app/src/main/java/com/vokkavpn/x/SplashActivity.java
+++ app/src/main/java/com/vokkavpn/x/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.vokkavpn.x;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -11,10 +12,20 @@ import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.vokkavpn.x.Util.Utils;
+
+import java.util.List;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -23,7 +34,7 @@ public class SplashActivity extends AppCompatActivity {
     private static int m_nWhatHandler = 1;
     private static long	m_lDelayTime = 2000;
     private TextView txtVersion;
-
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +51,11 @@ public class SplashActivity extends AppCompatActivity {
             pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
             String version = pInfo.versionName;
             txtVersion.setText("Version " + version);
+            requestPermission();
         } catch (PackageManager.NameNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        mHandler.sendEmptyMessageDelayed(m_nWhatHandler, m_lDelayTime);
 
     }
 
@@ -59,13 +69,51 @@ public class SplashActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            gotoNext();
-        }
-    };
 
+    private void requestPermission() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_PHONE_STATE
+                        )// you can add permissions here comma separated....
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted!", Toast.LENGTH_SHORT).show();
+
+                            mHandler = new Handler(){
+                               @Override
+                               public void handleMessage(Message msg) {
+                                   gotoNext();
+                               }
+                           };
+
+                            mHandler.sendEmptyMessageDelayed(m_nWhatHandler, m_lDelayTime);
+
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
 
     private void gotoNext(){
         if(Data.isConnectionDetails) {
